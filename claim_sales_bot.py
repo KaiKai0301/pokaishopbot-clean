4f8195aa74c377854c0cbf1e8194e8330cab725b
KaiKai0301 <kykee003@mymail.sim.edu.sg>
2025-10-21 04:35:23 +0800

    Fix syntax error in bot file

--- claim_sales_bot.py
+++ claim_sales_bot.py
@@ -3,31 +3,31 @@ import logging
 from telegram.ext import Updater, CommandHandler 
 from flask import Flask 
  
-logging.basicConfig\(level=logging.INFO\) 
-logger = logging.getLogger\(__name__\) 
+logging.basicConfig(level=logging.INFO) 
+logger = logging.getLogger(__name__) 
  
-app = Flask\(__name__\) 
+app = Flask(__name__) 
  
-@app.route\(\'/\'\) 
-def home\(\): 
-    return \'?? PokaiShop Bot is running!\' 
+@app.route('/') 
+def home(): 
+    return '?? PokaiShop Bot is running!' 
  
-def start_command\(update, context\): 
-    update.message.reply_text\(\'?? PokaiShop Bot is working!\'\) 
+def start_command(update, context): 
+    update.message.reply_text('?? PokaiShop Bot is working!') 
  
-def main\(\): 
-    token = os.environ.get\(\'BOT_TOKEN\'\) 
+def main(): 
+    token = os.environ.get('BOT_TOKEN') 
     if not token: 
-        logger.error\(\'? BOT_TOKEN not set\'\) 
+        logger.error('? BOT_TOKEN not set') 
         return 
  
-    updater = Updater\(token, use_context=True\) 
+    updater = Updater(token, use_context=True) 
     dispatcher = updater.dispatcher 
-    dispatcher.add_handler\(CommandHandler\(\'start\', start_command\)\) 
+    dispatcher.add_handler(CommandHandler('start', start_command)) 
  
-    logger.info\(\'?? Starting bot...\'\) 
-    updater.start_polling\(\) 
-    updater.idle\(\) 
+    logger.info('?? Starting bot...') 
+    updater.start_polling() 
+    updater.idle() 
  
-if __name__ == \'__main__\': 
-    main\(\) 
+if __name__ == '__main__': 
+    main() 
